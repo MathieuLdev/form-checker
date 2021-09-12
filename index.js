588301d26a5d8d208885db54390707263efb1f31
Mathieu <mathieu.linder.dev@gmail.com>
2021-09-12 16:02:38 +0200

    fin du js et css

--- index.js
+++ index.js
@@ -1,32 +1,93 @@
 const inputs = document.querySelectorAll('input[type="text"], input[type="email"], input[type="password"]');
+const progressBar = document.getElementById("progress-bar");
+let pseudo, email, password, confirmPass;
+const form = document.querySelector("form");
 
+// const pseudoChecker = (value) => {
+//     const pseudoContainer = document.querySelector('.pseudo-container');
+//     const errorDisplay = document.querySelector('.pseudo-container > span');
+
+//     if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+//         pseudoContainer.classList.add('error');
+//         errorDisplay.textContent = "Le pseudo doit faire entre 3 et 20 caractères";
+//     } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+//         pseudoContainer.classList.add('error');
+//         errorDisplay.textContent = "Le pseudo ne doit pas contenir de caractères spéciaux";
+//     } else {
+//         pseudoContainer.classList.remove('error');
+//         errorDisplay.textContent = "";
+//     }
+// };
+
+
+const errorDisplay = (tag, message, valid) => {
+    const container = document.querySelector("." + tag + "-container");
+    const span = document.querySelector("." + tag + "-container > span");
+
+    if (!valid) {
+        container.classList.add('error');
+        span.textContent = message ;
+    } else {
+        container.classList.remove('error');
+        span.textContent = message;
+    }
+}
 
 const pseudoChecker = (value) => {
-    const pseudoContainer = document.querySelector('.pseudo-container');
-    const errorDisplay = document.querySelector('.pseudo-container > span');
-
     if (value.length > 0 && (value.length < 3 || value.length > 20)) {
-        pseudoContainer.classList.add('error');
-        errorDisplay.textContent = "Le pseudo doit faire entre 3 et 20 caractères";
+        errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères", false);
+        pseudo = null;
     } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
-        pseudoContainer.classList.add('error');
-        errorDisplay.textContent = "Le pseudo ne doit pas contenir de caractères spéciaux";
+        errorDisplay("pseudo", "Le pseudo ne doit pas contenir de caractères spéciaux", false);
+        pseudo = null;
     } else {
-        pseudoContainer.classList.remove('error');
-        errorDisplay.textContent = "";
+        errorDisplay("pseudo", "", true)
+        pseudo = value;
     }
 };
 
 
-
 const emailChecker = (value) => {
-    console.log(value);
+    if (!value.match(/^[\w_.-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+        errorDisplay("email", "Le mail n'est pas valide", false);
+        email = null;
+    } else {
+        errorDisplay("email", "", true);
+        email = value;
+    }
 };
+
+
 const passwordChecker = (value) => {
-    console.log(value);
+    progressBar.classList = "";
+
+    if (!value.match(/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/)) {
+        errorDisplay("password", "Minimum de 8 caractères, un chiffre, une majuscule et un caractère spécial");
+        progressBar.classList.add('progressRed');
+        password = null;
+    } else if (value.length < 12) {
+        errorDisplay("password", "", true);
+        progressBar.classList.add('progressBlue');
+        password = value;
+    } else {
+        errorDisplay("password", "", true);
+        progressBar.classList.add('progressGreen');
+        password = value;
+    }
+    if (confirmPass) {
+        confirmChecker(confirmPass);
+    }
 };
+
+
 const confirmChecker = (value) => {
-    console.log(value);
+    if (value !== password) {
+        errorDisplay("confirm", "Les mots de passe ne correspondent pas", false);
+        confirmPass = false;
+    } else {
+        errorDisplay("confirm", "", true);
+        confirmPass = true;
+    }
 };
 
 
@@ -51,3 +112,29 @@ inputs.forEach((input) => {
         };
     });
 });
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (pseudo && email && password && confirmPass) {
+        const data = {
+            pseudo: pseudo,
+            email: email,
+            password: password,
+        };
+        console.log(data);
+
+        inputs.forEach((input) => {
+            (input.value = "");
+        });
+        progressBar.classList = "";
+        alert("Inscription validée");
+        pseudo = null;
+        email = null;
+        password = null;
+        confirmPass = null;
+    } else {
+        alert("Veuillez remplir correctement les champs");
+    }
+})
